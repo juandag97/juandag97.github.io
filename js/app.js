6e372e34a0ff9a5d573417610afad5d0f8e5bdbd
Juan David Acosta <juandag@unicauca.edu.co>
2024-10-20 12:35:41 -0500

    About me traduction enable

--- js/app.js
+++ js/app.js
@@ -18,7 +18,21 @@ const translations = {
         foodDeliveryAppTitle: "Aplicación para pedidos de comida",
         foodDeliveryAppContent: "Una aplicación móvil para entrega de comida permite a los usuarios explorar los menús, realizar pedidos y recibir las comidas. Las características principales incluyen una navegación fácil de usar, seguimiento de pedidos en tiempo real, opciones de comida personalizables y métodos de pago seguros. Está diseñada para una experiencia gastronómica cómoda y sin inconvenientes.",
         realStateAppTitle: "Aplicación para bienes raices",
-        realStateAppContent: "Esta aplicación móvil ayuda a los usuarios a encontrar la casa de sus sueños o la propiedad de inversión. Con funciones como filtros de búsqueda avanzados, mapas interactivos, recorridos virtuales y contacto con agentes, simplifica el proceso de compra, venta o alquiler de bienes raíces desde cualquier lugar."
+        realStateAppContent: "Esta aplicación móvil ayuda a los usuarios a encontrar la casa de sus sueños o la propiedad de inversión. Con funciones como filtros de búsqueda avanzados, mapas interactivos, recorridos virtuales y contacto con agentes, simplifica el proceso de compra, venta o alquiler de bienes raíces desde cualquier lugar.",
+//    About Me Values
+        myNameIs: "Hola, mi nombres es",
+        buildingThings: "Me apasiona crear soluciones con electrónica y software",
+        lastApp: "Descarga nuestra última aplicación: ",
+        myselfDetails: "Soy ingeniero electrónico y vivo en Popayán, Colombia. Como desarrollador fullstack, trabajo principalmente con los stacks MERN y MEAN, así como con el desarrollo móvil usando Flutter. Además, soy arquitecto de soluciones en la nube especializado en AWS y Azure. Utilizo herramientas de inteligencia artificial de vanguardia de los principales proveedores de la nube, incluidos Amazon Q, Amazon Bedrock, Azure AI Services, Azure OpenAI y Claude.",
+        aboutMe: "Acerca de mi",
+        hiProfession: "Hola, soy Juan, soy ingeniero electrónico y desarrollador de software basado en Popayán, Colombia.",
+        detailsParagraph1: "Me gusta crear cosas que vivan en Internet y que también generen valor para los usuarios. En línea con eso, me encanta trabajar con desarrollo de software, soluciones cloud, inteligencia artificial, ingeniería de datos y aprendizaje automático, ya que estas tecnologías son tendencia y transversales a varios campos.",
+        detailsParagraph2: "Me gusta tocar la guitarra, el piano y cantar. Además soy un amante de los autos, especialmente de los carros alemánes y los JDM como: el porsche GTR-RS 911, los bmw clase M y serie 3, el Nissan R34-Skyline, el Nissan GTR-NISMO, el Toyota Supra y el Subaru WRX. Ahora tengo un Chevrolet Sail llamado 'Apollo' (el de la foto). Espero crear un blog pronto, sobre tunning de autos, autos eléctricos, noticias de tecnología, desarrollo de software, inteligencia artificial y un poco de todo.",
+        getInTouch: "Ponte en contacto", 
+        despiteTheFact: "A pesar de que actualmente no estoy buscando nuevas oportunidades, mi bandeja de entrada siempre está abierta. Si tienes alguna pregunta o simplemente quieres saludarme, intentaré responderte. Saludos.",
+        sayHello: "Pongámonos en contacto",
+        main: "Inicio",
+        contact: "Contacto",
     },
     en: {
         firstHeadline: "Senior Fullstack Developer at Teleperformance",
@@ -39,7 +53,22 @@ const translations = {
         foodDeliveryAppTitle: "Food Delivery App",
         foodDeliveryAppContent: "A mobile app for food delivery allows users to browse menus, place orders, and have meals delivered. Key features include user-friendly navigation, real-time order tracking, customizable meal options, and secure payment methods. It's designed for a seamless and convenient dining experience.",
         realStateAppTitle: "Real State App",
-        realStateAppContent: "This mobile app helps users find their dream home or investment property. With features like advanced search filters, interactive maps, virtual tours, and agent contact, it simplifies the process of buying, selling, or renting real estate on the go."
+        realStateAppContent: "This mobile app helps users find their dream home or investment property. With features like advanced search filters, interactive maps, virtual tours, and agent contact, it simplifies the process of buying, selling, or renting real estate on the go.",
+        //    About Me Values
+        myNameIs: "Hi, my name is",
+        buildingThings: "I love to build interesting stuff with Electronics and Software",
+        lastApp: "Download our last app: ",
+        myselfDetails: " I am an electronic engineer residing in Popayán, Colombia. As a fullstack developer, I primarily work with the MERN and MEAN stacks, as well as mobile development using Flutter. In addition, I am a cloud solutions architect specialized in AWS and Azure. I utilize cutting-edge AI tools from leading cloud providers, including Amazon Q, Amazon Bedrock, Azure AI Services, Azure OpenAI, and Claude.",
+        aboutMe: "About me",
+        hiProfession: "Hi! I'm Juan, an electronic engineer and a software developer based in Popayán, Colombia.",
+        detailsParagraph1: "I enjoy creating things that live on the internet and also that generate value for the users. Inline with that I love to work with Data Analysis, Data Engineering, and Machine Learning, because these technologies generates new insights and relevant information, for several fields.",
+        detailsParagraph2: " like to play the guitar, the piano and sing. Plus I'm a car lover, especially of german and JDM cars like: the porsche 911 GT3-RS, bmw M Class and 3 series, the Nissan R34-Skyline, the Nissan GTR-NISMO, the Toyota Supra, and the Subaru WRX. Now I have a Chevrolet Sail called 'Apollo' (the one of the picture). I hope to build a blog soon, about car tunning, electric cars, technology news, software development, artificial intelligence, and a little bit of everything.",
+        getInTouch: "Get In Touch", 
+        despiteTheFact: "Despite the fact, I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question, or you just want to say hi, I'll try to reply you back. Cheers.",
+        sayHello: "Say Hello",
+        main: "Main",
+        contact: "Contact"
+    
     }
 };
 
@@ -78,4 +107,27 @@ function changeLanguagePortafolio(lang) {
         btn.classList.remove('active');
     });
     document.querySelector(`.lang-btn[onclick="changeLanguagePortafolio('${lang}')"]`).classList.add('active');
+}
+
+function changeLanguageAboutMe(lang){
+    document.getElementById('myNameIs').innerText = translations[lang].myNameIs;
+    document.getElementById('buildingThings').innerText = translations[lang].buildingThings;
+    document.getElementById('lastApp').innerText = translations[lang].lastApp;
+    document.getElementById('myselfDetails').innerText = translations[lang].myselfDetails;
+    document.getElementById('aboutMe').innerText = translations[lang].aboutMe;
+    document.getElementById('hiProfession').innerText = translations[lang].hiProfession;
+    document.getElementById('detailsParagraph1').innerText = translations[lang].detailsParagraph1;
+    document.getElementById('detailsParagraph2').innerText = translations[lang].detailsParagraph2;
+    document.getElementById('getInTouch').innerText = translations[lang].getInTouch;
+    document.getElementById('despiteTheFact').innerText = translations[lang].despiteTheFact;
+    document.getElementById('sayHello').innerText = translations[lang].sayHello;
+    document.getElementById('build').innerText = translations[lang].build + new Date().getFullYear();
+    document.getElementById('main').innerText = translations[lang].main;
+    document.getElementById('aboutMeNav').innerText = translations[lang].aboutMe;
+    document.getElementById('contact').innerText = translations[lang].contact;
+    
+    document.querySelectorAll('.lang-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    document.querySelector(`.lang-btn[onclick="changeLanguageAboutMe('${lang}')"]`).classList.add('active');
 }
